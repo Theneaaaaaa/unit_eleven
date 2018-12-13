7a10c6b0766a9f4f80b7f21290f3cb5cc49b37aa
Thenea <thenea@iMac4.local>
2018-12-13 13:50:55 -0500

    Making the block move

---
+++ warmup.py
@@ -0,0 +1,46 @@
+import pygame, sys
+from pygame.locals import *
+import block
+
+
+pygame.init()
+WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 500
+X_SPEED = 3
+Y_SPEED = 3
+main_window = pygame.display.set_mode((400, 500), 32, 0)
+pygame.display.set_caption("Animation")
+
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+WIDTH = 25
+HEIGHT = 25
+
+my_block = block.Block(main_window, WIDTH, HEIGHT, BLUE)
+
+# starting position of the block. Upper left corner of the screen
+my_block.rect.x = 10
+my_block.rect.y = 10
+main_window.blit(my_block.image, my_block.rect)
+pygame.display.update()
+
+
+while True:
+    for event in pygame.event.get():
+        if event == QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    main_window.fill(WHITE)
+    my_block.rect.left += X_SPEED
+    my_block.rect.top += Y_SPEED
+    if my_block.rect.left <= 0 or my_block.rect.right >= WINDOW_WIDTH:
+        X_SPEED = -X_SPEED
+    if my_block.rect.top <= 0 or my_block.rect.bottom >= WINDOW_HEIGHT:
+        Y_SPEED = -Y_SPEED
+    main_window.blit(my_block.image, my_block.rect)
+    pygame.display.update()
+
+
+
+
